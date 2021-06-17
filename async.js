6e61eac9e8d4c9711b1149be6568755d89dece0d
alibek-begnaev <alibek.begnaev@gmail.com>
2021-06-17 17:56:04 +0500

    aaa

--- async.js
+++ async.js
@@ -1,3 +1,9 @@
-cosnt;
-fsadfs;
-sdfasdfs;
+const first = () => console.log("1");
+const second = () => console.log("2");
+const third = () => console.log("3");
+
+first();
+
+setTimeout(second, 0);
+
+third();
